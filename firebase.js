2d75a13095a518113598c21587955a51466a99bb
Akshay Jadhav <83538162+Akshay-cybersec@users.noreply.github.com>
2024-03-10 12:59:07 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,13 +5,7 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCtXpZehwD7VPwBwCwm_UV_VskQqxjc1DQ",
-  authDomain: "campus-connect-123.firebaseapp.com",
-  projectId: "campus-connect-123",
-  storageBucket: "campus-connect-123.appspot.com",
-  messagingSenderId: "80139995557",
-  appId: "1:80139995557:web:ee37741c16c2ba59cec04c",
-  measurementId: "G-VF246DQ9YW"
+  // config file 
 };
 
 // Initialize Firebase
